e8e89a261698654faaf63f9786644aa2f023c0e5
Devan <2381012@unai.edu>
2025-02-10 21:05:00 +0700

    handson3

---
+++ handson 3/handson-3/src/App.tsx
@@ -0,0 +1,26 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p style={{ color: "red" }}>Hello world</p>
+      <table className="tabel">
+        <tr>
+          <th>Name</th>
+          <th>NIM</th>
+          <th>Address</th>
+          <th>Email</th>
+        </tr>
+
+        <tr>
+          <td>Devan Nusi</td>
+          <td>2381012</td>
+          <td>UNAI</td>
+          <td>2381012@unai.edu</td>
+        </tr>
+      </table>
+    </>
+  );
+}
+
+export default App;
